5c126dbbf10298ab4bfbbc0180475c1d4d40fc5f
Roma Joy <joynormahouser@gmail.com>
2022-08-27 16:06:12 -0500

    added controllers

---
+++ models/yarn.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+
+const yarnSchema = new mongoose.Schema({
+    brand: String,
+    name: String,
+    color: String,
+    yards: Number,
+    weight: String,
+    purchaseDate: Date,
+    skeins: Number,
+    store: String,
+    note: String
+})
+
+const Yarn = mongoose.model('Yarn', yarnSchema)
+module.exports = Yarn
