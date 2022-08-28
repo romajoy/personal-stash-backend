1c121cfe32a5dee8616058ecc0244816a06e8e73
Roma Joy <joynormahouser@gmail.com>
2022-08-27 19:00:05 -0500

    pushing heroku

--- server.js
+++ server.js
@@ -2,26 +2,39 @@ const express = require('express')
 const app = express()
 const mongoose = require ('mongoose')
 const cors = require('cors')
+require('dotenv').config()
 
+
+const db = mongoose.connection
+const MONGODB_URI = process.env.MONGODB_URI
+// Connect to Mongo
+
+mongoose.connect(MONGODB_URI, () => {
+    console.log('whatever')
+})
+
+const PORT = process.env.PORT || 3003;
+
+// Error / success
+db.on('error', (err) => console.log(err.message + ' is Mongod not running?'));
+db.on('connected', () => console.log('mongo connected: ', MONGODB_URI));
+db.on('disconnected', () => console.log('mongo disconnected'));
 
 // JSON Middleware
 app.use(express.json())
 app.use(cors())
 
+console.log(process.env)
+
 // Routing Controllers
 const yarnController = require('./controllers/yarncontroller.js')
-app.use(yarnController)
+app.use('/yarn', yarnController)
 
 const hookController = require('./controllers/hookcontroller.js')
-app.use(hookController)
+app.use('/hooks', hookController)
 
-// Mongoose Connection
-mongoose.connect('mongodb://localhost:27017/stash')
-mongoose.connection.once('open', () => {
-    console.log('connected to mongod!')
-})
 
 // Listening
-app.listen(3000, () => {
+app.listen(PORT, () => {
     console.log('lisening...')
 })
